53899d6e6c411e14608b94078843422d3e2b2fe5
Neil Sardesai <Neil@wireless-10-146-191-40.public.utexas.edu>
2017-03-08 16:00:35 -0600

    Slight code cleanup

--- WWDC Crowd Simulator 2017.playground/Contents.swift
+++ WWDC Crowd Simulator 2017.playground/Contents.swift
@@ -1,14 +1,18 @@
 import SpriteKit
 import PlaygroundSupport
 
+// Change these values to whatever you want
+let width = 480
+let height = 270
+
 // Set up containing view
-let spriteView = SKView(frame: CGRect(x: 0, y: 0, width: 480, height: 270))
+let spriteView = SKView(frame: CGRect(x: 0, y: 0, width: width, height: height))
 spriteView.showsDrawCount = true
 spriteView.showsNodeCount = true
 spriteView.showsFPS = true
 
 // Add main scene
-let scene = MainScene(size: CGSize(width: 480, height: 270))
+let scene = MainScene(size: CGSize(width: width, height: height))
 spriteView.presentScene(scene)
 
 // Show in Playground live view
